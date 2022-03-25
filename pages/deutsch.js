66900b2c6624be1b412990ea6ba77440f8a53b1e
Mariano Ryser <rysermariano@gmail.com>
2022-03-25 13:18:02 +0100

    lol sin comillas

--- pages/deutsch.js
+++ pages/deutsch.js
@@ -20,7 +20,7 @@ const Deutsch = () => {
       Das <b>Wahrzeichen</b> von Wien. Diese alte
       gotische Kirche wollen alle Besucher sehen. Wer Zeit hat, kann
       den Turm besteigen oder die Katakomben unter der Kirche besichtigen.
-      Die 'Pummerin' ist die grösste Glocke Österreichs.
+      Die Pummerin ist die grösste Glocke Österreichs.
       Sie hängt im Nordtrum un kann mit einem Aufzug erreicht 
       werden. Familie Huber ist <b>beeindruckt</b> Sie <b>zünden</b> in der Kirche 
       eine <b>Kerze</b> an und gehen weiter. 
