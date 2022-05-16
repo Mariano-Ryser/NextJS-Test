c1782ea751407d88cd807a010376d2a14f59cc5f
Mariano Ryser <rysermariano@gmail.com>
2022-05-17 00:08:03 +0200

    uff una banda de cambios nuevos, Funcionalidad cordinada.

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import styles from '../../styles/Footer.module.css'
 import Link from 'next/link';
 
 const Footer = () => {
   return (
-    <footer className='footer'>
-        Nas
+    <footer className={styles.footer}>
+        <p className={styles.parrafo}>2m2r19</p>
     </footer>
   )
 }
--- pages/presentation.js
+++ pages/presentation.js
@@ -14,28 +14,49 @@ const Presentation = () => {
     setDeg(deg ? rot1 : rot2)
   }
 
+
+  const [showAlien, setShowAlien] = useState(false);
+  function ShowmeAlien(){
+    setShowAlien(!showAlien);
+  }
+
   return (
 <div className={styles.presentation}>
     
-    <h1 style={{transform:"rotateZ("+ deg + "deg)"}} className={styles.plus} onClick={Showme} ><Image
+    <div style={{transform:"rotatez("+ deg + "deg)"}}
+     className={styles.plus} onClick={Showme} ><Image
         src='/cruz.png'
         alt="bicho"
         width={50}
         height={50}
-      /></h1>
+      /></div>
+
+{
+        showAlien && (
+          <div className={styles.textoBox}>
+             <p className={styles.titulo}>Titulaso</p>
+             <p className={styles.parrafo}>-Parrafaso lorem</p>
+          </div>
+        )
+      }
 
    
   {/*CONTACTO, CONTACTO, CONTACTO, CONTACTO, CONTACTO, CONTACTO, CONTACTO, */}
+    
+    
     <div className={styles.contact}>
 
     {show &&(
-        <div className={styles.mobil}
+        <div 
         //  style={{display:"none"}}
          >
             <a href="https://github.com/Mariano-Ryser"><div className={styles.github}></div></a>
             <a href="https://www.instagram.com/marianoryser/"><div className={styles.insta}></div></a>
             <a href="https://www.linkedin.com/in/mariano-ryser-073731221/"><div className={styles.linkedin}></div></a>
             <a href="https://twitter.com/RyserMariano"><div className={styles.twiter}></div></a>
+            <div 
+            onClick={ShowmeAlien}
+             className={styles.alien}></div>
         </div>
     )
    }
