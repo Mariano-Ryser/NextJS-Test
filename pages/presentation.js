236d62e06f6993a5d3698fc151e7e3850e7ef7fe
Mariano Ryser <rysermariano@gmail.com>
2022-05-19 12:30:25 +0200

    vanilla js! en next! xD

--- pages/presentation.js
+++ pages/presentation.js
@@ -20,6 +20,26 @@ const Presentation = () => {
     setShowAlien(!showAlien);
   }
 
+  // ABRE EL MENU DE PROYECTOS
+function wantanamera(){
+  var boxIcons = document.getElementById("box")
+
+  if(boxIcons.style.transform === "translateX(0%)"){
+    
+    boxIcons.style.transform = "translateX(-100%)";
+    setTimeout(() => {
+      boxIcons.style.display = "none"
+  }, 300);
+ 
+  }else {
+    boxIcons.style.display = "block"
+    setTimeout(() => {
+      boxIcons.style.transform = "translateX(0%)";
+  }, 150);
+    
+    }
+}
+
   return (
 <div className={styles.presentation}>
     
@@ -31,15 +51,53 @@ const Presentation = () => {
         height={50}
       /></div>
 
-{
+        {/* CAJA DE TEXTO NEGRA */}
+      {
         showAlien && (
           <div className={styles.textoBox}>
-             <p className={styles.titulo}>Titulaso</p>
-             <p className={styles.parrafo}>-Parrafaso lorem</p>
+             <p className={styles.titulo}>No entiendo porque mierda..</p>
+             <p className={styles.parrafo}>-No puedo usar JS VANILLA en Next... estas cosas me hacen cuestionar todo.
+             ptmqmp.. uno quiere codear en paz y pisa este lodo de lentitud q te raja la cabeza.
+             <br></br>
+             Porejemplo:
+             <br></br>
+             var boxIcons = document.getElementById("box");
+                          
+            <br></br> Ya eso me putea el next.
+            <br></br>
+              
+            Si sabes como mierda puedo hacer, dejalo en tu comentario. o llamame por telefono.
+            al +7829124u23t426349170 mandame un imail.
+
+            Listo despues de probar y probar lo solicione!
+
+            pongamos la prueba.
+
+            <br></br>Porejemplo
+            <br></br>
+            <div 
+            onClick={wantanamera}
+             className={styles.bicho2}></div>
+              El faltante era la ubicacion de la variable que llama al id del obejeto.
+              que tiene que ir siempre adentro de la funcion especificada en la accion.
+              <br></br>
+              "var boxIcons = document.getElementById("box")"
+
+             sin importar utilizar el mismo logo con su funcionalidad,
+             manipulamos estados atravez de preguntas."
+             <br></br>
+
+
+             </p>
           </div>
         )
       }
-
+    
+    {/* CAJA DE PRUEBA JS VANILLA */}
+    <div id='box'
+    className={styles.box}>
+          
+          </div>
    
   {/*CONTACTO, CONTACTO, CONTACTO, CONTACTO, CONTACTO, CONTACTO, CONTACTO, */}
     
@@ -57,6 +115,9 @@ const Presentation = () => {
             <div 
             onClick={ShowmeAlien}
              className={styles.alien}></div>
+             <div 
+            onClick={wantanamera}
+             className={styles.bicho}></div>
         </div>
     )
    }
