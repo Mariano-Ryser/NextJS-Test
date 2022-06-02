b49412a927cea6ec9301ab97c3c92230932b97e6
Mariano Ryser <rysermariano@gmail.com>
2022-06-02 21:34:03 +0200

    azp!

--- pages/presentation.js
+++ pages/presentation.js
@@ -94,6 +94,10 @@ function wantanamera(){
         <div className={styles.glass}
         //  style={{display:"none"}}
          >
+             <a href="https://github.com/Mariano-Ryser"><div className={styles.github}></div></a>
+             <a href="https://www.instagram.com/marianoryser/"><div className={styles.insta}></div></a>
+            <a href="https://www.linkedin.com/in/mariano-ryser-073731221/"><div className={styles.linkedin}></div></a>
+            <a href="https://soundcloud.com/"><div className={styles.sound}></div></a> 
             <div 
             onClick={ShowmeAlien}
              className={styles.alien}></div>
@@ -104,13 +108,9 @@ function wantanamera(){
             // onClick={wantanamera}
              className={styles.paint}></div>
              </a>
-             {/* <a href="https://github.com/Mariano-Ryser"><div className={styles.github}></div></a>
-             <a href="https://www.instagram.com/marianoryser/"><div className={styles.insta}></div></a>
-            <a href="https://www.linkedin.com/in/mariano-ryser-073731221/"><div className={styles.linkedin}></div></a>
-            <a href="https://soundcloud.com/"><div className={styles.sound}></div></a> */}
         
-            {/* <a href="/Virtualdj"><div className={styles.twiter}></div></a> 
-            <a href="/soundclound"><div className={styles.twiter}></div></a>  */}
+             {/* <a href="/Virtualdj"><div className={styles.twiter}></div></a>  */}
+            <a href="/soundclound"><div className={styles.twiter}></div></a>  
            
         </div>
     )
